6d58bb37e527d4d2ffc48d55413739d6217e4182
Luke Czoboriek <LC169782@protonmail.com>
2021-05-17 16:18:36 +0100

    new commit

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,13 @@
+require 'pg'
+
+
 class Bookmark
   
 
   def self.all
-    [
-      "BBC","The Guardian","ITV"
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec("SELECT * FROM bookmarks;")
+    result.map { |bookmark| bookmark['url'] }
   end
 
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,12 +1,11 @@
 require 'bookmark'
 
-describe Bookmark do
-  
-  context 'bookmark_list' do
-    it 'returns a list of bookmarks' do
-      bookmarks = Bookmark.all
-      expect(bookmarks).to eq ["BBC","The Guardian","ITV"]
-    end
+describe '.all' do
+  it 'returns a list of bookmarks' do
+    bookmarks = Bookmark.all
 
+    expect(bookmarks).to include "http://www.makersacademy.com"
+    expect(bookmarks).to include "http://www.destroyallsoftware.com"
+    expect(bookmarks).to include "http://www.google.com"
   end
 end
