eb7e918880e796b4718941175631d906c3030ac0
Adam R <adamroyal12@gmail.com>
2021-05-20 16:17:26 +0100

    little bit refactored

--- spec/features/updating_a_bookmark_spec.rb
+++ spec/features/updating_a_bookmark_spec.rb
@@ -7,12 +7,13 @@ feature 'updating a bookmark' do
 
     first('.bookmark').click_button 'Update'
     expect(current_path).to eq "/bookmarks/#{bookmark.id}/update"
+
     fill_in('url', with: 'http://www.google.com')
     fill_in('title', with: 'Google')
     click_button('Submit')
+    
     expect(current_path).to eq '/bookmarks'
     expect(page).not_to have_link('Makers Academy', href: 'http://www.makersacademy.com')
     expect(page).to have_link('Google', href: 'http://www.google.com')
   end
 end
-
